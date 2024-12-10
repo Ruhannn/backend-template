6c172c2c43cde498cfd600353ed85953f32a8c51
Ruhannn <ruhanrouf17@gmail.com>
2024-12-11 02:52:28 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express, { json } from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+import { config } from 'dotenv';
+import { errorHandler, notFound } from './middlewares';
+import router from './api';
+
+
+config();
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+app.use(json());
+
+app.get('/', (req, res) => {
+    res.send('i love ayaka 😊');
+});
+
+
+app.use('/api', router);
+
+app.use(notFound);
+app.use(errorHandler);
+export default app;
---
+++ src/middlewares.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from 'express';
+import { config } from 'dotenv';
+config();
+export function notFound(req: Request, res: Response, next: NextFunction) {
+    res.status(404);
+    const error = new Error(`🔍 - Not Found - ${req.originalUrl}`);
+    next(error);
+}
+
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+export function errorHandler(err: Error, req: Request, res: Response, _next: NextFunction) {
+    const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+    res.status(statusCode);
+    res.json({
+        message: err.message,
+        ...(process.env.NODE_ENV !== 'production' && { stack: err.stack }),
+    });
+}
